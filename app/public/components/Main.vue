0447804c973fd24084337f802f7b982ab68cf472
gatinul <13252716435@163.com>
2018-01-20 15:18:42 +0800

    :sparkles: 博客封面

--- app/public/components/Main.vue
+++ app/public/components/Main.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="container">
-        <div>{{msg}}</div>
-        <Timeline>
-            <TimelineItem>
-                <p class="time">1976年</p>
-                <p class="content">Apple I 问世</p>
+        <div class="header_title">
+            <h2>. Gatinul</h2>
+            <p class="title_extra">
+                <Icon type="social-github"></Icon>
+                <Icon type="social-twitter"></Icon>            
+            </p>
+        </div>
+        <div class="left_title">{{left_title}}</div>
+        <Timeline class="timeline">
+            <TimelineItem color="red">
+                <p class="time">2017年</p>
+                <p class="content">The Begin Of All.</p>
             </TimelineItem>
             <TimelineItem>
-                <p class="time">1984年</p>
-                <p class="content">发布 Macintosh</p>
+                <p class="time">NOW</p>
+                <p class="content">There Are Some Describtion.</p>
             </TimelineItem>
-            <TimelineItem>
-                <p class="time">2007年</p>
-                <p class="content">发布 iPhone</p>
-            </TimelineItem>
-            <TimelineItem>
-                <p class="time">2010年</p>
-                <p class="content">发布 iPad</p>
-            </TimelineItem>
-            <TimelineItem>
-                <p class="time">2011年10月5日</p>
-                <p class="content">史蒂夫·乔布斯去世</p>
+            <TimelineItem color="green">
+                <p class="time">More</p>
+                <p class="content">There Are Some Describtion.</p>
             </TimelineItem>
         </Timeline>
+        <div class="foot_bd">进入博客&nbsp; >></div>
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
-                msg:'Hello'
+                left_title:'我的博客'
             }
         },
         created(){
@@ -46,6 +46,63 @@
         padding-left: 5px;
     }
     .container{
-        background-image: url('/public/images/white.png')
+        height: 100vh;
+        background: no-repeat url('/public/images/book.png');
+        background-size: 100% 100%;
+        opacity: 0.9;
+        position: relative;
+    }
+    .header_title{
+        position: absolute;
+        left: 42vw;
+        top:10vh;
+        /* font-style: italic; */
+    }
+    .header_title h2{
+        font-size: 48px;
+        font-weight: 800;
+    }
+    .header_title .title_extra {
+        font-size: 24px;
+        text-align: center;
+    }
+    .title_extra i {
+        margin-left: 3px;
+        cursor: pointer;
+    }
+    .left_title{
+        position: absolute;
+        top: 49vh;
+        left: 40vw;
+        transform:rotate(-12deg);
+        -ms-transform:rotate(-12deg); 	/* IE 9 */
+        -moz-transform:rotate(-12deg); 	/* Firefox */
+        -webkit-transform:rotate(-12deg); /* Safari 和 Chrome */
+        -o-transform:rotate(-12deg); 
+        width: 10px;
+        font-size: 22px;
+    }
+    .timeline{
+        position: absolute;
+        top:41vh;
+        left:53vw;
+        transform:rotate(-13.5deg);
+        -ms-transform:rotate(-13.5deg); 	/* IE 9 */
+        -moz-transform:rotate(-13.5deg); 	/* Firefox */
+        -webkit-transform:rotate(-13.5deg); /* Safari 和 Chrome */
+        -o-transform:rotate(-13.5deg); 	
+    }
+    .ivu-timeline-item{
+        padding: 0 0 28px 0;
+    }
+    .foot_bd{
+        position: absolute;
+        bottom: 5vh;
+        right: 5vw;
+        font-size: 18px;
+        font-weight: 500;
+        font-style: italic;
+        cursor: pointer;
+        color: #555;
     }
 </style>
